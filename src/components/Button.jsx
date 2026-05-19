d45e304c1d5dc963e3298b393ce2940f214ae9d7
Lucas José <lucasjosefariasilva@gmail.com>
2026-05-19 16:11:28 -0300

    Deploy do portifolio - header quase pronto

---
+++ src/components/Button.jsx
@@ -0,0 +1,20 @@
+
+const Button = ({text, className, id}) => {
+    return (
+        <a href="" className={`${className ?? ''} cta-wrapper`}>
+            <div className="cta-button group">
+                <div className="bg-circle" />
+                    <p className="text">{text}</p>
+                    <div className="arrow-wrapper">
+                        <img src="/images/arrow-down.svg" alt="arrow" />
+
+                    </div>
+
+                {/* </div> */}
+
+            </div>
+        </a>
+    )
+}
+
+export default Button
