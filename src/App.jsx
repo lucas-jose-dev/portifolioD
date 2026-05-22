784004b950f4782d493f6de4569d050eb1ab7c1c
Lucas José <lucasjosefariasilva@gmail.com>
2026-05-21 21:18:34 -0300

    Foi adicionada seção trabalho, carrocel de logos e cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Hero from './sections/Hero.jsx'
 import ShowcaseSection from './sections/ShowcaseSection.jsx'
+import Navbar from './components/HeroMoldels/NavBar.jsx'
+import LogoSection from './sections/LogoSection.jsx'
+import FeatureCards from './sections/FeatureCards.jsx'
 
 const App = () => {
     return (
         <>
+            <Navbar />
             <Hero />
             <ShowcaseSection />
+            <LogoSection />
+            <FeatureCards />
         </>
     )
 }
