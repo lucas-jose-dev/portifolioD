0c13ba9a11aa68d90c64e520d61d8192face6a79
Lucas José <lucasjosefariasilva@gmail.com>
2026-05-28 02:01:34 -0300

    Arrumando bug no mobile

--- src/components/HeroMoldels/HeroExperience.jsx
+++ src/components/HeroMoldels/HeroExperience.jsx
@@ -8,7 +8,7 @@ import Particles from "./Particles"
 
 const HeroExperience = () => {
     const isTablet = useMediaQuery({ query: '(max-width: 1024px)' })
-    const isMobile = useMediaQuery({ query: '(max-width: 76px)' })
+    const isMobile = useMediaQuery({ query: '(max-width: 768px)' })
 
     return (
         <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
